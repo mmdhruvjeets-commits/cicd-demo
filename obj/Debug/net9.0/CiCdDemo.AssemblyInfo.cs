e963d4230780ac8e4c10bf0c91071ea7f7af121a
Krusha Shah <krusha.s@gormalone.com>
2026-01-07 19:01:48 +0530

    New File added

--- obj/Debug/net9.0/CiCdDemo.AssemblyInfo.cs
+++ obj/Debug/net9.0/CiCdDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CiCdDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdfb35f4826ee6f6a1411486598435a0fc1291b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CiCdDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CiCdDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
